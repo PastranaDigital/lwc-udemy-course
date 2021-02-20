68aac0f33d3aab85f462b28eb353bc0126aaa4e0
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-02-19 22:50:46 -0600

    changed variable to non-primitive

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -2,33 +2,36 @@ import { LightningElement, track } from "lwc";
 
 export default class BmiCalculator extends LightningElement {
   cardTitle = "BMI Calculator";
-  weight;
-  height;
-  @track bmiCalculation;
+  // using track for this because it is not a primitive variable
+  @track bmiData = {
+    weight: 0,
+    height: 0,
+    result: 0
+  };
   alertDivClass;
 
   getBMIButtonHandler() {
-    if (this.weight != null && this.height != null) {
-      this.bmiCalculation = (
-        (703 * this.weight) /
-        (this.height * this.height)
+    if (this.bmiData.weight != 0 && this.bmiData.height != 0) {
+      this.bmiData.result = (
+        (703 * this.bmiData.weight) /
+        (this.bmiData.height * this.bmiData.height)
       ).toFixed(2);
-      if (this.bmiCalculation >= 30) {
+      if (this.bmiData.result >= 30) {
         this.alertDivClass = "high";
-      } else if (this.bmiCalculation <= 20) {
+      } else if (this.bmiData.result <= 20) {
         this.alertDivClass = "low";
       } else {
         this.alertDivClass = "medium";
       }
-      console.log("value: ", this.bmiCalculation);
+      console.log("value: ", this.bmiData.result);
     }
   }
 
   onWeightChangeHandler(event) {
-    this.weight = event.target.value;
+    this.bmiData.weight = event.target.value;
   }
 
   onHeightChangeHandler(event) {
-    this.height = event.target.value;
+    this.bmiData.height = event.target.value;
   }
 }
