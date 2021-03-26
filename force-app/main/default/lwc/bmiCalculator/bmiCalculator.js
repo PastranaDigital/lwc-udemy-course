a63005c9a2e5e3a213a944af4a161359a1330a74
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-03-26 12:52:22 -0500

    Lesson 34 - Share Javascript Code

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -1,4 +1,6 @@
 import { LightningElement, track } from "lwc";
+// this common javascript code can be used in any LWC you want
+import { getBMI } from "c/bmi";
 
 export default class BmiCalculator extends LightningElement {
   cardTitle = "BMI Calculator";
@@ -11,20 +13,16 @@ export default class BmiCalculator extends LightningElement {
   alertDivClass;
 
   getBMIButtonHandler() {
-    if (this.bmiData.weight !== 0 && this.bmiData.height !== 0) {
-      this.bmiData.result = (
-        (703 * this.bmiData.weight) /
-        (this.bmiData.height * this.bmiData.height)
-      ).toFixed(2);
-      if (this.bmiData.result >= 30) {
-        this.alertDivClass = "high";
-      } else if (this.bmiData.result <= 20) {
-        this.alertDivClass = "low";
-      } else {
-        this.alertDivClass = "medium";
-      }
-      console.log("value: ", this.bmiData.result);
+    this.bmiData.result = getBMI(this.bmiData.weight, this.bmiData.height);
+
+    if (this.bmiData.result >= 30) {
+      this.alertDivClass = "high";
+    } else if (this.bmiData.result <= 20) {
+      this.alertDivClass = "low";
+    } else {
+      this.alertDivClass = "medium";
     }
+    console.log("value: ", this.bmiData.result);
   }
 
   onWeightChangeHandler(event) {
