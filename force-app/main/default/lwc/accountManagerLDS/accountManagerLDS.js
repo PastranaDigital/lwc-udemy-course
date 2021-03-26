3630c00143521e8590169742715a59499bbfb38a
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-03-26 16:24:00 -0500

    37 View Record

--- force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
+++ force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
@@ -1,10 +1,18 @@
-import { LightningElement } from "lwc";
-import { createRecord } from "lightning/uiRecordApi";
+import { LightningElement, wire } from "lwc";
+import { createRecord, getRecord } from "lightning/uiRecordApi";
 
+const fieldArray = ["Account.Name", "Account.Phone", "Account.Website"];
 export default class AccountManagerLDS extends LightningElement {
   accountName;
   accountPhone;
   accountWebsite;
+
+  recordId;
+
+  // getRecord( recordId, fieldToRetrieve )
+  // if we used this. the property would not be reactive
+  @wire(getRecord, { recordId: "$recordId", fields: fieldArray })
+  accountRecord;
 
   accountNameChangeHandler(event) {
     this.accountName = event.target.value;
@@ -32,9 +40,31 @@ export default class AccountManagerLDS extends LightningElement {
     createRecord(recordInput)
       .then((response) => {
         console.log("Account has been created : ", response.id);
+        this.recordId = response.id;
       })
       .catch((error) => {
         console.log("Error in creating account : ", error.body.message);
       });
+  }
+
+  get retAccountName() {
+    if (this.accountRecord.data) {
+      return this.accountRecord.data.fields.Name.value;
+    }
+    return undefined;
+  }
+
+  get retAccountPhone() {
+    if (this.accountRecord.data) {
+      return this.accountRecord.data.fields.Phone.value;
+    }
+    return undefined;
+  }
+
+  get retAccountWebsite() {
+    if (this.accountRecord.data) {
+      return this.accountRecord.data.fields.Website.value;
+    }
+    return undefined;
   }
 }
