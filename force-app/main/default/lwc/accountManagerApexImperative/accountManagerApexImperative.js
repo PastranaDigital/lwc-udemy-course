c39551b7a66f36808371f6a432f7c95d30e96f67
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-03-29 06:53:21 -0500

    42. Display A Toast Notification

--- force-app/main/default/lwc/accountManagerApexImperative/accountManagerApexImperative.js
+++ force-app/main/default/lwc/accountManagerApexImperative/accountManagerApexImperative.js
@@ -1,5 +1,6 @@
 import { LightningElement } from "lwc";
 import getAccounts from "@salesforce/apex/AccountManagerImperative.getAccounts";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class AccountManagerApex extends LightningElement {
   numberOfRecords;
@@ -20,9 +21,31 @@ export default class AccountManagerApex extends LightningElement {
     getAccounts({ numberOfRecords: this.numberOfRecords })
       .then((response) => {
         this.accounts = response;
+        if (this.accounts.length < this.numberOfRecords) {
+          const toastEvent = new ShowToastEvent({
+            title: "Accounts Loaded... but not as many as requested",
+            message:
+              "Only Found " + this.accounts.length + " Accounts From Server",
+            variant: "warning"
+          });
+          this.dispatchEvent(toastEvent);
+        } else {
+          const toastEvent = new ShowToastEvent({
+            title: "All Accounts Loaded",
+            message: this.numberOfRecords + " Accounts Fetched From Server",
+            variant: "success"
+          });
+          this.dispatchEvent(toastEvent);
+        }
       })
       .catch((error) => {
         console.error("Error getting the accounts", error.body.message);
+        const toastEvent = new ShowToastEvent({
+          title: "ERROR",
+          message: error.body.message,
+          variant: "error"
+        });
+        this.dispatchEvent(toastEvent);
       });
   }
 }
