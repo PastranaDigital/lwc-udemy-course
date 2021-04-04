623651e9fca75e63c20eb87d7e2587c71341269a
“Omar <omarpastrana@Omars-MBP.lan>
2021-04-03 21:00:52 -0500

    59. Develop CarDetails Component

--- force-app/main/default/lwc/carTile/carTile.js
+++ force-app/main/default/lwc/carTile/carTile.js
@@ -1,8 +1,12 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import { CurrentPageReference } from "lightning/navigation";
+import { fireEvent } from "c/pubsub";
 
 export default class CarTile extends LightningElement {
   @api car; // coming from the carSearch component
   @api carSelectedId;
+
+  @wire(CurrentPageReference) pageRef;
 
   carSelectHandler(event) {
     // the default action of this anchor tag will be prevented
@@ -12,6 +16,9 @@ export default class CarTile extends LightningElement {
 
     const carSelect = new CustomEvent("carselect", { detail: carId });
     this.dispatchEvent(carSelect);
+
+    // sends information to the carDetails component
+    fireEvent(this.pageRef, "carselect", this.car.Id);
   }
 
   get isCarSelected() {
