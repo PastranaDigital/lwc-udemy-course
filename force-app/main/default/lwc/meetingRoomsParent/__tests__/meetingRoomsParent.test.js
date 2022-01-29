214d18ee807f63ea4975057cef649ec0fb99b7ab
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2022-01-29 05:47:06 -0600

    completed Jest test module

--- force-app/main/default/lwc/meetingRoomsParent/__tests__/meetingRoomsParent.test.js
+++ force-app/main/default/lwc/meetingRoomsParent/__tests__/meetingRoomsParent.test.js
@@ -7,37 +7,35 @@ describe("c-meetingRoomsParent", () => {
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
     }
+  });
 
-    // one of the tests for this LWC
-    it("count the number of meeting room component iterations", () => {
-      // create the component
-      const meetingRooms = createElement("c-meetingRoomsParent", {
-        is: MeetingRoomsParent
-      });
-
-      document.body.appendChild(meetingRooms);
-      // stores all of the meeting room components
-      const allMeetingRoomComponents = meetingRooms.shadowRoot.querySelectorAll(
-        "c-meeting-room"
-      );
-      // count the number of components & assert this value
-      expect(allMeetingRoomComponents.length).toBe(7); // could also use 'not to be'
+  // one of the tests for this LWC
+  it("count the number of meeting room component iterations", () => {
+    // create the component
+    const meetingRooms = createElement("c-meetingRoomsParent", {
+      is: MeetingRoomsParent
     });
 
-    it("check the title of the lightning-card", () => {
-      // create the component
-      const meetingRooms = createElement("c-meetingRoomsParent", {
-        is: MeetingRoomsParent
-      });
+    document.body.appendChild(meetingRooms);
+    // stores all of the meeting room components
+    const allMeetingRoomComponents = meetingRooms.shadowRoot.querySelectorAll(
+      "c-meeting-room"
+    );
+    // count the number of components & assert this value
+    expect(allMeetingRoomComponents.length).toBe(7); // could also use 'not to be'
+  });
 
-      document.body.appendChild(meetingRooms);
-
-      const lightningCardComponent = meetingRooms.shadowRoot.querySelector(
-        "lightning-card"
-      );
-      expect(lightningCardComponent.getAttribute("title")).toBe(
-        "Meeting Rooms"
-      );
+  it("check the title of the lightning-card", () => {
+    // create the component
+    const meetingRooms = createElement("c-meetingRoomsParent", {
+      is: MeetingRoomsParent
     });
+
+    document.body.appendChild(meetingRooms);
+
+    const lightningCardComponent = meetingRooms.shadowRoot.querySelector(
+      "lightning-card"
+    );
+    expect(lightningCardComponent.title).toBe("Meeting Rooms");
   });
 });
